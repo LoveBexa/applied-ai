133d63d49c2129fc4acdc8f27f2045c46a20d6c0
Bexa <bexa@bexas-mac-mini.home>
2022-02-23 23:30:09 +0000

    Completed tabu search on a knapsack bag

--- Tabu Search/knapsack-tabu-search.py
+++ Tabu Search/knapsack-tabu-search.py
@@ -7,7 +7,134 @@ pd.set_option('display.max_colwidth', None)
 pd.set_option('display.max_rows', None)
 
 # Converting csv to data frame. Total weight = 2621
-data = pd.read_csv("knapsack.csv")
+data = pd.read_csv("/Users/bexa/Documents/Computer Science/Applied AI/Code.py/Data Files/knapsack.csv")
+
+data_true = data
+data_true["true value"] = data_true["values"] / data_true["weights"]
 
 # Add another column for v/w 
 
+# Item list 1-100
+items = random.sample(range(1, 101), 100)
+
+########### Calculating bag items ###########
+
+def calculateTotalWeight(bag):
+    total_weight = 0
+    for item in bag:
+    # Iterate through dataframe of bag items
+        for index, row in data_true.iterrows():
+            if (item == index):
+                total_weight += row["weights"]
+    return total_weight
+
+def calculateTotalValue(bag):
+    total_value = 0
+    for item in bag:
+    # Iterate through dataframe of bag items
+        for index, row in data_true.iterrows():
+            if (item == index):
+                total_value += row["values"]
+    return total_value
+
+def calculateWeight(item):
+        for index, row in data_true.iterrows():
+            if (item == index):
+                return row["weights"]
+            
+def calculateValue(item):
+        for index, row in data_true.iterrows():
+            if (item == index):
+                return row["values"]
+    
+def calculateTrueValue(item):
+        for index, row in data_true.iterrows():
+            if (item == index):
+                return row["true value"]
+            
+def leftovers(bag):
+    current_bag = bag
+    # Total items from 1 to 100
+    total_items = items
+    # Items - current bag = leftover bag 
+    leftovers = [i for i in total_items if i not in current_bag]
+    return leftovers
+
+
+########### Knapsack Bag Initialisations ########### 
+
+
+# random_start = random.sample(range(1, 101), 100)
+
+random_start =[ 13, 74, 98, 50, 31, 89, 2, 12, 68, 65, 96, 93, 25, 40, 78, 59, 15, 55, 19, 62, 75, 9, 35, 37, 48, 79, 87, 82, 20, 28, 72, 45, 77, 38, 23, 32, 85, 70, 42, 10, 54, 90, 44, 58, 56, 94, 41, 92, 8, 1, 5, 69, 29, 46]
+
+
+starting_best = random_start.copy()
+best_solution = random_start.copy()
+
+tabu_list = []
+tabu_list.append(random_start)
+
+# How many iterations the of 
+iterations = 3
+
+bag_leftovers =  leftovers(random_start)
+bag_weight = calculateTotalWeight(random_start)
+bag_value = calculateTotalValue(random_start)
+
+bag_capacity = 1500 
+
+print(bag_weight, bag_value)
+
+def generateNeighbours(solution):
+    neighbour_solutions = []
+    counter = 0
+    # Deep copy array
+    while counter < iterations: 
+        bag_copy = solution.copy()
+        leftover_copy = leftovers(solution).copy()
+        # Grab random item from current bag
+        random_bag = random.choice(bag_copy)
+        # Grab a random item from the leftover bag
+        random_leftover = random.choice(leftover_copy)
+        # Remove and add new item
+        bag_copy.remove(random_bag)
+        bag_copy.append(random_leftover)
+        # Remove it from the leftover bag
+        leftover_copy.remove(random_leftover)
+        neighbour_solutions.append(bag_copy)
+        counter += 1
+    return neighbour_solutions
+    
+def bestNeighbour(neighbourhood):
+    best_neighbour = random_start
+    for solution in neighbourhood:
+        if (calculateTotalValue(solution) > calculateTotalValue(best_neighbour)) and (calculateTotalWeight(solution) <= bag_capacity):
+            best_neighbour = solution
+        else:
+            continue
+    print(calculateTotalWeight(best_neighbour), calculateTotalValue(best_neighbour), best_neighbour)
+    return best_neighbour
+    
+def tabuSearch():
+    global best_solution, starting_best
+    
+    i = 0
+    
+    while iterations < 5:
+        # Use best candidate as start solution
+        neighbourhood = generateNeighbours(best_solution)
+        best_solution = bestNeighbour(neighbourhood)
+        for solution in neighbourhood:
+            if solution not in tabu_list and calculateTotalValue(solution) > calculateTotalValue(best_solution):
+                best_solution = solution
+                tabu_list.append(solution)
+                
+        if calculateTotalValue(best_solution) > calculateTotalValue(starting_best):
+            starting_best = best_solution
+        
+        if len(tabu_list) > iterations:
+            tabu_list.pop(0)
+        i += 1
+        
+tabuSearch()
---
+++ Tabu Search/tsp-tabu-search.py
@@ -0,0 +1,186 @@
+import pandas as pd
+import numpy as np
+import math, random
+
+# Show full width of dataframe
+pd.set_option('display.max_colwidth', None)
+# pd.set_option('display.max_rows', None)
+
+# Converting csv to data frame and adding headers
+headers = ['X', 'Y']
+data = pd.read_csv("/Users/bexa/Documents/Computer Science/Applied AI/Code.py/Data Files/TSP-Matrix.csv", names=["X","Y"])
+
+print(data)
+# Calculates the euclidean distance between 2 points 
+def calculatePath(first, second):
+    # Shift index by +1 because starts from 0
+    first -= 1
+    second -= 1
+    x_sq = ((data["X"].iloc[second] - data["X"].iloc[first])) ** 2
+    y_sq = ((data["Y"].iloc[second] - data["Y"].iloc[first])) ** 2
+    euclidean =  x_sq + y_sq
+    length = math.sqrt(euclidean)
+    return length
+    
+########### Step 1 Initial Solution: Greedy approach for first route ########### 
+
+greedy_start = []
+# Create a list of 1 to 24 (cities) everytime one is appended to greedy_start one is removed from the list of cities array
+cities = list(range(1, 25))
+current_path = greedy_start
+
+def remove(city):
+    # Every time we append a new city to greedy we pop it from cities array.
+    greedy_start.append(city)
+    cities.remove(city)
+
+# Start at start_city, add to array
+start_city = 4
+start_city_next = 8
+start_cost = calculatePath(start_city, start_city_next)
+# Add first 2 starting points to greedy start and remove from Cities array. 
+remove(start_city)
+remove(start_city_next)
+
+# Pointer on the current city!
+current_city = start_city_next
+
+########### Choose the next best City by calulating all possible routes from current node and selecting minimum cost option ########### 
+
+def nextBest(current):
+# Create a dict for current_city > next_city and all the length values
+
+    city_costs = {}
+# Iterate through each item in cities array
+    for key, city in enumerate(cities):
+        # Calculate distance from each city in array to the current_city
+        route_cost = calculatePath(current, city)
+        # append each of these cities and their cost to a dictionary
+        city_costs[city] = route_cost
+        
+    city_costs = city_costs.items()
+    city_costs_2 = list(city_costs)
+    # Convert to a dataframe
+    city_costs_3 = pd.DataFrame(city_costs_2)
+    city_costs_3.columns = ["City","Cost"]
+    # Pick the smallest cost
+    min_cost = city_costs_3[city_costs_3.Cost == city_costs_3.Cost.min()]
+    # next_city = shortest_route city
+    next_city = int(min_cost["City"])
+    # Return the shortest length
+    return next_city
+
+# Iterates through the length of city
+while len(cities) > 0:
+    # The next city is the shortest calculated route cost
+    next_city = nextBest(current_city)
+    # Add next best city stop off to the greedy start array and then remove it from the cities list
+    remove(next_city)
+    
+
+
+########### Neighbourhood operator: Do a swap of two adjacent cities. However, you are encouraged to experiment with other neighbourhood operators. ########### 
+
+
+print("Greedy Start = ", greedy_start)
+greedy_cost = 0
+        # For each city 
+for city in greedy_start:
+    # Find the index
+    city_index = greedy_start.index(city)
+    if city_index < 23:
+        # Then count up the cost from A -> B, B -> C etc
+        greedy_cost = greedy_cost + calculatePath(greedy_start[city_index], greedy_start[city_index+1])
+    else:
+        # Then make sure we add on the cost of the last city back to the first
+        greedy_cost = greedy_cost + calculatePath(greedy_start[23], greedy_start[0])
+print("Cost = ",greedy_cost)
+
+
+# Random swap!
+
+def swap(route):
+    new_route = route.copy()
+    city1 = random.randint(0, 23)
+    city2 = random.randint(0, 23)
+    # They must not be the same city as you can't swap these
+    if city1 != city2:
+        new_route[city1], new_route[city2] = new_route[city2], new_route[city1]
+    else: # If they are the same then go to another city
+        city1 -= 1
+        new_route[city1], new_route[city2] = new_route[city2], new_route[city1]
+    return new_route
+
+
+########### Add the cost of travelling through the cities ########### 
+
+def calculateRoute(route):
+    cost_total = 0
+        # For each city     
+    for key, city in enumerate(route):
+        # Find the index
+        if route.index(city) < 23:
+            # Then count up the cost from A -> B, B -> C etc
+            this_city = route[key]
+            next_city = route[key+1]
+            cost_total = cost_total + calculatePath(this_city, next_city)
+        else:
+            # Then make sure we add on the cost of the last city back to the first
+            cost_total = cost_total + calculatePath(route[23], route[0])
+
+    return round(cost_total, 2)
+
+
+
+########### Stopping criteria: No improvement in consecutive 100 iterations or how ever many ########### 
+
+# There are 24 neihbouring cities therefor there are 24-1 neighbouring solutions
+swaps = 40
+count = 0
+
+columns = list(range(1, 25))
+solutions = {}
+
+best_route = greedy_start
+
+while swaps > count:
+    # Cost of current best route
+    best_route_cost = calculateRoute(best_route)
+    # Create new route!!
+    new_route = swap(best_route)
+    # Calculate cost of new route!!
+    new_cost = calculateRoute(new_route)
+    # If new route is better than best route then make into best route
+    if new_cost < best_route_cost:
+        best_route = new_route   
+    else:
+        best_route = best_route
+        
+    solutions[calculateRoute(best_route)] = best_route
+    count += 1
+    
+# Convert dictionary to a pandas Data Frame 
+solutions_items = solutions.items()
+solution_list = list(solutions_items)
+solutions_df = pd.DataFrame(solution_list, columns= ["Cost","Solution"])
+solutions_df.index += 1
+
+print(solutions_df)
+
+# # Sort by lowest cost
+# best_solution = solutions_df.sort_values(by=['Cost'])
+# #print(best_solution.head(24))
+# print(solutions_df)
+# # Print the best solution!!!!!!! 
+solutions_df['Cost'] = pd.to_numeric(solutions_df['Cost'])
+
+print("\nThe best solution out of the neighbourhood is solution number", solutions_df['Cost'].idxmin())
+print(solutions_df.min())
+
+
+# Draw route
+# ax1 = data.plot.scatter(x='X',y='Y', c='Pink')
+# plot.show(block=True);
+# data['Z'] = data['Y'] / data['X']
+# data_sort = data.sort_values(by=['Z'])
+# print(data)
